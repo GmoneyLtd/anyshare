beb626c1d6efbdfeb81741643acf65db3ea39fbe
apuer <apuer@gmoney.ltd>
2026-05-14 22:29:00 +0800

    fix dockerfile healthcheck error for server bind v6 address

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import secrets
 
 class Config:
     # 应用配置
-    HOST = os.getenv("HOST", "0.0.0.0")
+    HOST = os.getenv("HOST", "[::]")
     PORT = int(os.getenv("PORT", 8000))
     THREADS = int(os.getenv("THREADS", os.cpu_count() * 2 + 1 if os.cpu_count() else 4))
     LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
